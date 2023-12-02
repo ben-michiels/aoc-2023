5669498ab755fd3dacf9318308248b878eb1d5e1
Ben <ben.michiels@gmail.com>
2023-12-02 20:08:13 +0000

    Add remaining day 2 files

---
+++ 02-12/1/Challenge.py
@@ -0,0 +1,26 @@
+import re
+
+redRegex = "(\d+) red"
+greenRegex = "(\d+) green"
+blueRegex = "(\d+) blue"
+
+lines = open('../input.txt').read().splitlines()
+
+count = 0;
+
+for index, line in enumerate(lines):
+  redMatches = re.findall(redRegex, line, re.MULTILINE)
+  if any(int(match) > 12 for match in redMatches):
+    continue
+
+  greenMatches = re.findall(greenRegex, line, re.MULTILINE)
+  if any(int(match) > 13 for match in greenMatches):
+    continue
+
+  blueMatches = re.findall(blueRegex, line, re.MULTILINE)
+  if any(int(match) > 14 for match in blueMatches):
+    continue
+
+  count = count + (index + 1)
+
+print(count)
