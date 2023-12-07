a9c4104b0c3f2d3fe8ae3c82a755b0b96ed3a54f
Ben Michiels <bmichiels@caci.co.uk>
2023-12-07 11:47:16 +0000

    Day 7 complete

---
+++ 07-12/1/Challenge.py
@@ -0,0 +1,64 @@
+import re
+
+lines = open('../input.txt').read().splitlines()
+
+cardRankings = {
+  "2": 1,
+  "3": 2,
+  "4": 3,
+  "5": 4,
+  "6": 5,
+  "7": 6,
+  "8": 7,
+  "9": 8,
+  "T": 9,
+  "J": 10,
+  "Q": 11,
+  "K": 12,
+  "A": 13,
+}
+
+rounds = []
+
+class Round:
+  def __init__(self, hand, bid):
+    self.hand = hand
+    self.bid = bid
+    self.handRank = 0
+    self.cardRanks = []
+
+def getHandRank(r):
+  cardDict = {}
+
+  for card in r.hand:
+    if card in cardDict:
+      cardDict[card] += 1
+    else:
+      cardDict[card] = 1
+
+  matchingCards = []
+  for cardType in cardDict:
+    matchingCards.append(cardDict[cardType])
+  matchingCards.sort(reverse=True)
+
+  return matchingCards
+
+for line in lines:
+  lineParts = line.split(" ")
+  r = Round(lineParts[0], int(lineParts[1]))
+
+  for card in r.hand:
+    r.cardRanks.append(cardRankings[card])
+
+  r.handRank = getHandRank(r)
+  
+  rounds.append(r)
+
+rounds = sorted(rounds, key=lambda self: self.cardRanks)
+rounds = sorted(rounds, key=lambda self: self.handRank)
+
+totalWinnings = 0
+for rIdx, r in enumerate(rounds):
+  totalWinnings += (r.bid * (rIdx + 1))
+
+print(totalWinnings)
---
+++ 07-12/2/Challenge.py
@@ -0,0 +1,71 @@
+import re
+
+lines = open('../input.txt').read().splitlines()
+
+cardRankings = {
+  "J": 1,
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "T": 10,
+  "Q": 11,
+  "K": 12,
+  "A": 13,
+}
+
+rounds = []
+
+class Round:
+  def __init__(self, hand, bid):
+    self.hand = hand
+    self.bid = bid
+    self.handRank = 0
+    self.cardRanks = []
+
+def getHandRank(r):
+  cardDict = {}
+
+  for card in r.hand:
+    if card in cardDict:
+      cardDict[card] += 1
+    else:
+      cardDict[card] = 1
+
+  matchingCards = []
+  for cardType in cardDict:
+    if cardType != "J":
+      matchingCards.append(cardDict[cardType])
+  matchingCards.sort(reverse=True)
+
+  if("J" in cardDict):
+    if(len(matchingCards) > 0):
+      matchingCards[0] += cardDict["J"]
+    else:
+      matchingCards.append(cardDict["J"])
+
+  return matchingCards
+
+for line in lines:
+  lineParts = line.split(" ")
+  r = Round(lineParts[0], int(lineParts[1]))
+
+  for card in r.hand:
+    r.cardRanks.append(cardRankings[card])
+
+  r.handRank = getHandRank(r)
+  
+  rounds.append(r)
+
+rounds = sorted(rounds, key=lambda self: self.cardRanks)
+rounds = sorted(rounds, key=lambda self: self.handRank)
+
+totalWinnings = 0
+for rIdx, r in enumerate(rounds):
+  totalWinnings += (r.bid * (rIdx + 1))
+
+print(totalWinnings)
